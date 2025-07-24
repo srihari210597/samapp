9cb6805e7882e2f701cbdd558717279f9b452525
Srihari <srihari@1cloudhub.com>
2025-07-24 12:01:50 +0530

    v4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '✅ Hello from ECS-v1!'
+    return '✅ Hello from ECS-v3!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
